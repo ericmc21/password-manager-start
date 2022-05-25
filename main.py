6c54f6ec76c039fef2053a7b60b60f71492d4ab6
Eric McLean <eric.mclean@infermedica.com>
2022-05-24 21:07:51 -0400

    added json storage and search

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-
     website = web_site_entry.get()
     email = email_user_entry.get()
     password = password_entry.get()
@@ -43,16 +42,31 @@ def save():
                 data = json.load(file)
         except FileNotFoundError:
             with open("data.json", "w") as file:
-                json.dump(data, file, indent=4)
+                json.dump(new_data, file, indent=4)
         else:
             data.update(new_data)
 
             with open("data.json", "w") as file:
-                json.dump(data, file, indent=4)
+                json.dump(new_data, file, indent=4)
         finally:
             web_site_entry.delete(0, END)
             password_entry.delete(0, END)
 
+
+def search():
+    website = web_site_entry.get()
+    try:
+        with open("data.json") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -67,9 +81,12 @@ canvas.grid(column=1, row=0)
 web_site_label = Label(text="Website:")
 web_site_label.grid(column=0, row=1)
 
-web_site_entry = Entry(width=35)
-web_site_entry.grid(column=1, row=1, columnspan=2)
+web_site_entry = Entry(width=21)
+web_site_entry.grid(column=1, row=1)
 web_site_entry.focus()
+
+search_button = Button(text="Search", command=search, width=13)
+search_button.grid(column=2, row=1)
 
 email_user_label = Label(text="Email/Username:")
 email_user_label.grid(column=0, row=2)
